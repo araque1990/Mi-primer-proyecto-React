95b9ef1150ddf07385e7027aa80f3010e2d9e75e
Cristian Araque <araque1990.cdap@gmail.com>
2022-11-02 10:50:38 +0100

    Utilizo el hook useEffect, en los comentarios explico su funcionamiento

--- src/components/ComponenteClase/Calculadora.jsx
+++ src/components/ComponenteClase/Calculadora.jsx
@@ -1,13 +1,25 @@
 // hook useState; este nos permite usar el estado de los componentes funcionales
 // useState es una funcion que nos retorna un array y este arreglo contiene dos valores,
 // el estado del componente y una funcion que nos permite actualizar ese estado
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Calculadora = ({ nombre }) => {
 
   const [state, setState] = useState({
     numero1: 0, numero2: 0
   })
+
+  //useEffect se ejecuta cuando el componente va a ser construido
+  //el segundo parametro es un array opcional.
+  //El array será el listado de todos los estados que quiero controlar como disparadores de la funcion que escribí en el useEffect
+  //si no le paso array como segundo parametro el useEffect vuelve a correr siempre que algun estado se actualice
+  //si le paso un array vacio, el useEffect no vuelve a correr
+  useEffect(() => {
+    console.log(new Date().getTime())
+    return () => {
+      //Aquí va todo lo que quieras ejecutar cuando el componente se desmonte
+    }
+  },[])
   
   const setNumber1 = (e) => {
     setState ({
